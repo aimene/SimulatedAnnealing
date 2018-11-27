095af71b9dfd42c5448db8f7a4d0c650a7ca6c1f
Aimene Amiour <aimeneamiour@gmail.com>
2018-11-28 00:24:41 +0100

    constructor SetUpParams DONE

--- SA/SetUpParams.h
+++ SA/SetUpParams.h
@@ -9,11 +9,16 @@ namespace simulatedAnnealing {
   	//ici vous devez mettre quelques paramètres tels que :
 
 	private:
-		static const unsigned int   _independent_runs = 2000000;         //number of independent runs
-		static const unsigned int   _nb_evolution_steps = 30 ;       // number of iterations per run
+		static const unsigned int   _max_independent_runs = 2000000;         //number of independent runs
+		static const unsigned int   _max_evolution_steps = 30 ;       // number of iterations per run
 
+		unsigned int _nb_independent_runs;
+		unsigned int _nb_evolution_steps;
+
+		double _temperature ;
+		
 		//unsigned int   _population_size;		// number of solutions in the population
-		unsigned int   _solution_size;	        // size of each particle
+		//unsigned int   _solution_size;	        // size of each particle
 
 	public:
 		SetUpParams ();
@@ -21,10 +26,20 @@ namespace simulatedAnnealing {
  		friend ostream& operator<< (ostream& os, const SetUpParams& setup);
 		friend istream& operator>> (istream& is, SetUpParams& setup);
 
-		const unsigned int   independent_runs() const;
+		const unsigned int   max_independent_runs() const;
+		const unsigned int   max_evolution_steps() const;
+
+		const unsigned int   nb_independent_runs() const;
 		const unsigned int   nb_evolution_steps() const;
-		const unsigned int   population_size() const;
+
+		void update_temperature(const double& x) ;
+		void setup_temperature(const double& x);
+
+		bool is_less__max_independent_runs()const;
+		bool is_less_max_evolution_steps()const;
+
 		const unsigned int   solution_size() const;
+
 
 		~SetUpParams();
   };
