ac71649234ef922a28cf2cc0c51dd65d4f67b908
Aimene Amiour <aimeneamiour@gmail.com>
2018-11-28 00:16:29 +0100

    constructor setUpParam  done

---
+++ SA/SetUpParams.cpp
@@ -0,0 +1,10 @@
+#include "pch.h"
+#include "SetUpParams.h"
+namespace simulatedAnnealing {
+
+	simulatedAnnealing::SetUpParams::SetUpParams() : _nb_evolution_steps{},
+		_nb_independent_runs{}, _temperature{}{}
+
+
+
+}
