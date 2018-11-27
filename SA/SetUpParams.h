62e6122305d9c291179b0c03d83f80239c4b292c
Aimene Amiour <aimeneamiour@gmail.com>
2018-11-28 00:14:09 +0100

    constructor setUpParam  done

--- SA/SetUpParams.h
+++ SA/SetUpParams.h
@@ -3,14 +3,16 @@
 #include<ostream>
 
 namespace simulatedAnnealing {
+	using namespace std;
       class SetUpParams
   {
   	//ici vous devez mettre quelques paramètres tels que :
 
 	private:
-		unsigned int   _independent_runs;         //number of independent runs
-		unsigned int   _nb_evolution_steps;       // number of iterations per run
-		unsigned int   _population_size;		// number of solutions in the population
+		static const unsigned int   _independent_runs = 2000000;         //number of independent runs
+		static const unsigned int   _nb_evolution_steps = 30 ;       // number of iterations per run
+
+		//unsigned int   _population_size;		// number of solutions in the population
 		unsigned int   _solution_size;	        // size of each particle
 
 	public:
--- SA/myAlgorithm.cpp
+++ SA/myAlgorithm.cpp
@@ -1,3 +1,12 @@
 #include"MyAlgorithm.h"
+
 namespace simulatedAnnealing {
+	Solution & MyAlgorithm::solution(const unsigned int index) const
+	{
+		// TODO: insérer une instruction return ici
+	}
+
+	MyAlgorithm::MyAlgorithm(const Problem & pbm, const SetUpParams & setup)
+	{}
+
 }
