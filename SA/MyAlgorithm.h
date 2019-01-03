947292d5eeaae684ad9d031ca1cc9fe14987c745
Aimene Amiour <aimeneamiour@gmail.com>
2019-01-03 19:43:59 +0100

    doxygen fait pour la classe myAlgorithme

--- SA/MyAlgorithm.h
+++ SA/MyAlgorithm.h
@@ -15,64 +15,104 @@ namespace simulatedAnnealing {
 
 
 	using namespace std;
+
+//=======================================================================
+/**
+ * \file       MyAlgorith.h
+ * \author     Preyankan Anandharoopan
+ * \version    1.7
+ * \date       13 Novembre 2015
+ * \brief      Classe représentant le noyau de l'algorithme
+ */
   class MyAlgorithm
   {
 	private:
-		//vector<Solution*> _solutions;     // individuals in population
-		//vector<struct particle> _fitness_values;
-		
-		//unsigned int _upper_cost,_lower_cost; // lower and upper fitness of individuals in population
 
+		/**
+		* \brief	les paramètres de l'algorithme  SA
+		*/
 	    SetUpParams& _setup;
+
+		/**
+		* \brief	le problème de l'algorithme 
+		*/
 		Problem _problem;
+
+		/**
+		* \brief	la solution courante dans un moment choisi
+		*/
 		Solution _current_solution;
+
+		/**
+		* \brief	la meilleur solution tirer pendant l'exécution de l'algorithme  SA
+		*/
 		Solution _best_solution;
 
 		
 
 	public:
+
+		/**
+		* \brief	Construit un MyAlgorithm
+		* \details	Initialise les valeurs  : 	_problem{ pbm }, _setup{ setup }, _current_solution{_problem},_best_solution{_problem}		
+		* \param    pbm \e Problem problème
+		* \param    setup \e SetUpParams setup
+		*/
 		MyAlgorithm(const Problem& pbm, SetUpParams& setup);
+
+		/**
+		* \brief	détruit MyAlgorithm
+		*/
 		~MyAlgorithm();
 
+		/**
+		* \brief	renvoie une référence vers l'objet qui contient les paramètres de l'algorithme SA
+		* \return   Un \e SetUpParams représentant une référence vers l'objet qui contient les paramètres de l'algorithme  SA
+		*/
+		const SetUpParams& get_setup() const;
 
-	
+		/**
+		* \brief	renvoie une référence vers _best_solution qui contient la meilleur solution trouvé 
+		* \return   Un \e Solution représentant une référence vers  _best_solution
+		*/
+		const Solution& get_best_solution() const;
 
-	  	bool stop_condition();
+		/**
+		* \brief	renvoie une référence vers _current_solution qui contient la solution  courante
+		* \return   Un \e Solution représentant une référence vers  _current_solution
+		*/
+		const Solution& get_current_solution() const;
 
-		void updateSolution(Solution* sol);
+		/**
+		* \brief	renvoie une référence vers _problem qui contient le problem à optimiser
+		* \return   Un \e Problem représentant une référence vers  _problem
+		*/
+		const Problem& get_problem() const;
 
-		
-
-		const SetUpParams& setup() const;
-		
-
-		Solution get_best_solution() const;
-
-		void solve();
-
+		/**
+		* \brief  lance l'exécution de l'algorithme SA
+		*/
 		void run();
 
+		/**
+		* \brief  résout le problème en trouvant une solution optimale
+		*/
+		void solve();
+
+		/**
+		* \brief  mis à jour la solution courante et la meilleure solution en fonction de la loi de metropolis
+		*/
+		void updateSolution(Solution* sol);
+		
+		/**
+		* \brief    surcharge d'opérateurs <<
+		* \details  permet d'afficher les variables à l'écran
+		*
+		* \param    os \e ostream flux de sortie
+		* \param    myAlgo \e MyAlgorithm algorithme
+		*/
 		friend ostream& operator<< (ostream& os, const MyAlgorithm& myAlgo);
-		friend istream& operator>> (istream& is, MyAlgorithm& myAlgo);
-		MyAlgorithm& operator= (const MyAlgorithm& myAlgo);
 
-		// creates a array with fitness of all solutions in MyAlgorithm and its position in the MyAlgorithm
-       /* void evaluate();
-
-	 	const vector<Solution*>& solutions() const;
-		unsigned int upper_cost() const;
-		unsigned int lower_cost() const;
-		Solution& solution(const unsigned int index) const;
-        vector<struct particle>&  fitness_values();
-		double fitness(const unsigned int index) const;
-
-
-		double best_cost() const;
-		double worst_cost() const;
-		Solution& best_solution() const;
-		Solution& worst_solution() const;
-
-		void evolution(int iter); /*makes an evolution step*/
 
   };
 }
--- SA/myAlgorithm.cpp
+++ SA/myAlgorithm.cpp
@@ -4,6 +4,12 @@
 namespace simulatedAnnealing {
 
 
+	/**
+	* \brief	Construit un MyAlgorithm
+	* \details	Initialise les valeurs  : 	_problem{ pbm }, _setup{ setup }, _current_solution{_problem},_best_solution{_problem}
+	* \param    pbm \e Problem problème
+	* \param    setup \e SetUpParams setup
+	*/
 	MyAlgorithm::MyAlgorithm(const Problem & pbm,  SetUpParams & setup) :
 		_problem{ pbm }, _setup{ setup }, _current_solution{_problem}
 	,_best_solution{_problem}
@@ -11,91 +17,106 @@ namespace simulatedAnnealing {
 		
 	}
 
+	/**
+	* \brief	détruit MyAlgorithm
+	*/
 	MyAlgorithm::~MyAlgorithm()
 	{
 	}
 
-	const SetUpParams & MyAlgorithm::setup() const
+	/**
+	* \brief	renvoie une référence vers l'objet qui contient les paramètres de l'algorithme SA
+	* \return   Un \e SetUpParams représentant une référence vers l'objet qui contient les paramètres de l'algorithme  SA
+	*/
+	const SetUpParams & MyAlgorithm::get_setup() const
 	{
 		return _setup;
 
 	}
 
-	bool MyAlgorithm::stop_condition()
-	{
-		return _setup.is_less_max_evolution_steps() || _setup.is_less__max_independent_runs();
-	}
-
-	
-		
-
-	
-
-
-	
-
-	Solution MyAlgorithm::get_best_solution() const
+	/**
+	* \brief	renvoie une référence vers _best_solution qui contient la meilleur solution trouvé
+	* \return   Un \e Solution représentant une référence vers  _best_solution
+	*/
+	const Solution& MyAlgorithm::get_best_solution() const
 	{
 		return _best_solution;
 	}
 
+	/**
+	* \brief	renvoie une référence vers _current_solution qui contient la solution  courante
+	* \return   Un \e Solution représentant une référence vers  _current_solution
+	*/
+	const Solution & MyAlgorithm::get_current_solution() const
+	{
+		return _current_solution;
+	}
+	/**
+		* \brief	renvoie une référence vers _problem qui contient le problem à optimiser
+		* \return   Un \e Problem représentant une référence vers  _problem
+	*/
+	const Problem & MyAlgorithm::get_problem() const
+	{
+		return _problem;
+	}
 
+	/**
+	* \brief  lance l'exécution de l'algorithme SA
+	*/
 	void MyAlgorithm::run()
 	{
 		_best_solution = _current_solution;
 		while (_setup.is_less__max_independent_runs())
 		{
+			std::cout << "================= run( " << _setup.get_nb_independent_runs() << " )=====================" << endl;
+			std::cout << _best_solution << std::endl;
 
 			_setup.setup_temperature();
 			solve();
 			_setup.increment_nb_independent_runs();
+
+			std::cout << _best_solution << std::endl;
 		}
 	}
 
+	/**
+	* \brief  résout le problème en trouvant une solution optimale
+	*/
 	void MyAlgorithm::solve()
 
 	{
-		std::cout << "================= run( "<<_setup.get_nb_independent_runs() <<" )=====================" << endl;
 		
-		std::cout << _best_solution << std::endl;
-	
-		
-
 		Solution* sol = new Solution{ _best_solution };
 
 		while (_setup.is_less_max_evolution_steps())
 		{
-		if (_setup.get_current_temperature() < 0) break;
+			if (_setup.get_current_temperature() < 0) break;
 
-		updateSolution(sol);
+			updateSolution(sol);
 		
-		delete sol;
+			delete sol;
 
-		sol = _problem.random_solution(_current_solution);
+			sol = _problem.random_solution(_current_solution);
 
-		sol->fitness();
+			sol->fitness();
 
-		_setup.update_temperature();
-		_setup.increment_nb_evolution_steps();
+			_setup.update_temperature();
+			_setup.increment_nb_evolution_steps();
 
 		}
+		delete sol;
 		_setup.set_nb_evolution_steps(0);
-
-
-		std::cout << _best_solution << std::endl;
 
 	}
 
-	
-
+	/**
+	* \brief  mis à jour la solution courante et la meilleure solution en fonction de la loi de metropolis
+	*/
 	void MyAlgorithm::updateSolution(Solution* sol)
 	{
-
 	
 	      	double proba = 0.0;
-	
-
-			
+				
 			if (sol->get_fitness() > _current_solution.get_fitness())
 			{
 				proba = exp(-1 * (_current_solution.get_fitness()- sol->get_fitness()) /_setup.get_current_temperature());
@@ -118,11 +139,21 @@ namespace simulatedAnnealing {
 				_best_solution = _current_solution;			
 			}
 				
-			
-		
-			
-		
-		//cout << "=====================================updateSolution---END==================================" << endl;
+	}
+
+   /**
+	* \brief    surcharge d'opérateurs <<
+	* \details  permet d'afficher les variables à l'écran
+	*
+	* \param    os \e ostream flux de sortie
+	* \param    myAlgo \e MyAlgorithm algorithme
+	*/	
+	ostream & operator<<(ostream & os, const MyAlgorithm & myAlgo)
+	{
+		os << myAlgo.get_problem() << endl;
+		os << myAlgo.get_setup() << endl;
+		os << myAlgo.get_current_solution() << endl;
+		os << myAlgo.get_best_solution() << endl;
 	}
 
 }
